ac0e984940feaeed9b1f8636eddd8848c3bbc269
Mayank Sharma <mash07.dev@gmail.com>
2022-06-27 18:44:39 +0530

    error logging

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,11 @@ import Hook from './Hook';
   // Hook into the iframe Console
   Hook(window.console, true);
 
+  // Log JS errors
+  window.addEventListener('error', ({ error }) => {
+    console.error(error);
+  });
+
   // Clear console on IIFE invocation
   parent.postMessage(
     {
